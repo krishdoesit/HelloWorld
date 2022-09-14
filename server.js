b3c32b4adc45215e8a39187b1a1e4fb3398e83c3
krishdoesit <khpatel28@myseneca.ca>
2022-09-14 19:43:23 -0400

    final update

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Krish Harshadkumar patel Student ID: 123898215 Date: 2022/09/14
+*
+*  Cyclic Web App URL: https://purple-rhinoceros-tam.cyclic.app/
+*
+*  GitHub Repository URL: https://github.com/krishdoesit/Helloworld
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
